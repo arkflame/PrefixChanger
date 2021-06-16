05150ce0c0711ce790390e17c90cbdf700c9e4b1
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-15 22:53:39 -0300

    add playerjoinlistener

---
+++ src/dev/_2lstudios/prefixchanger/listeners/PlayerJoinListener.java
@@ -0,0 +1,12 @@
+package dev._2lstudios.prefixchanger.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoinListener implements Listener {
+    @EventHandler
+    public void onPlayerJoin(final PlayerJoinEvent event) {
+        
+    }
+}

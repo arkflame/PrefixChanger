1e754ee1449729669653340daffd94e223d51afa
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-15 22:53:10 -0300

    add prefixcommand

---
+++ src/dev/_2lstudios/prefixchanger/commands/PrefixCommand.java
@@ -0,0 +1,13 @@
+package dev._2lstudios.prefixchanger.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class PrefixCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}

155d667af3b1437a86eae7ef792fcad9202b5cea
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-15 22:53:03 -0300

    add main plugin class

---
+++ src/dev/_2lstudios/prefixchanger/PrefixChanger.java
@@ -0,0 +1,10 @@
+package dev._2lstudios.prefixchanger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PrefixChanger extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}

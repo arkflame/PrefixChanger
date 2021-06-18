165536cecb12aaab60c1dbdb64589011beaa7fc8
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-06-17 21:44:02 -0300

    add autogenerated methods

--- src/dev/_2lstudios/prefixchanger/database/YamlPrefixDatabase.java
+++ src/dev/_2lstudios/prefixchanger/database/YamlPrefixDatabase.java
@@ -1,5 +1,40 @@
 package dev._2lstudios.prefixchanger.database;
 
-public class YamlPrefixDatabase {
-    
+import java.util.Collection;
+import java.util.UUID;
+
+import org.bukkit.entity.Player;
+
+import dev._2lstudios.prefixchanger.prefix.Prefix;
+
+public class YamlPrefixDatabase implements PrefixDatabase {
+    @Override
+    public Collection<Prefix> getPrefixes() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Prefix getPrefixByName(String prefixName) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Prefix getPrefixByPlayer(String uuidString) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Prefix getPrefixByPlayer(UUID uuid) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Prefix getPrefixByPlayer(Player player) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }

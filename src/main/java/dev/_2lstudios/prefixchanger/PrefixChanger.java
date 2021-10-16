b9d1bea3990e08014aa1a428f14e2b19223e7bac
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-10-16 16:37:28 -0300

    register prefixchanger placeholder

--- src/main/java/dev/_2lstudios/prefixchanger/PrefixChanger.java
+++ src/main/java/dev/_2lstudios/prefixchanger/PrefixChanger.java
@@ -8,6 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import dev._2lstudios.prefixchanger.commands.PrefixCommand;
 import dev._2lstudios.prefixchanger.entities.Prefix;
 import dev._2lstudios.prefixchanger.entities.PrefixPlayer;
+import dev._2lstudios.prefixchanger.placeholderapi.PrefixChangerPlaceholder;
 
 public class PrefixChanger extends JavaPlugin {
     @Override
@@ -15,6 +16,8 @@ public class PrefixChanger extends JavaPlugin {
         MilkshakeORM.connect(DatabaseType.MONGODB, "mongodb://localhost/database-name");
         MilkshakeORM.addRepository(Prefix.class);
         MilkshakeORM.addRepository(PrefixPlayer.class);
+
+        new PrefixChangerPlaceholder(this).register();
 
         getCommand("prefixchanger").setExecutor(new PrefixCommand());
     }
